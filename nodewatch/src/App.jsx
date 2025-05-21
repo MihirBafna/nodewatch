2e1197fd3a9d91cae1bca93a20308cff732e91e9
MihirBafna <mihir.bafna@gmail.com>
2025-05-21 00:44:10 -0400

    added cpus and timestamp bottom right

--- nodewatch/src/App.jsx
+++ nodewatch/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import {
-  Box,
+  Alert,Tabs,
   SimpleGrid,
   Heading,
   Text,
@@ -8,27 +8,20 @@ import {
   Spinner,
   Slider,
   Card,
+  Box,
+  Separator,
   Stack,
   Status,
   Image,
   HStack,
-  Flex
+  Flex,
+  Badge
 } from "@chakra-ui/react";
 
-import { TbColumns1 } from "react-icons/tb";
-import { LuColumns4 } from "react-icons/lu";
-
-
-const HOSTS = [
-  "minotaur",
-  "cyclops",
-  "asura",
-  "dementor",
-  "kappa",
-  "charybdis",
-  "wookiee",
-  "imp",
-];
+const GPU_HOSTS = ["minotaur", "cyclops", "asura", "dementor", "kappa", "charybdis", "wookiee", "imp"];
+// const CPU_HOSTS = ["oni", "yokai", "satyr", "golem", "kyogre", "dragon", "godzilla", "giant"];
+const CPU_HOSTS = [];
+const HOSTS = [...GPU_HOSTS, ...CPU_HOSTS];
 
 
 function getGPURowColor(gpu) {
@@ -41,8 +34,17 @@ function getGPURowColor(gpu) {
   return "#076301";                       
 }
 
+function sumCPU(cpuUsage) {
+  return Object.values(cpuUsage).reduce((a, b) => a + b, 0);
+}
+
+function sumMem(memUsage) {
+  return Object.values(memUsage).reduce((a, b) => a + b, 0);
+}
+
 function App() {
   const [gpuData, setGpuData] = useState({});
+  const [cpuData, setCpuData] = useState({});
   const [lastUpdated, setLastUpdated] = useState({});
   const [columnCount, setColumnCount] = useState(2);
 
@@ -52,7 +54,14 @@ function App() {
         fetch(`http://${host}.csail.mit.edu:9988/gpustat`)
           .then((res) => res.json())
           .then((data) => {
-            setGpuData((prev) => ({ ...prev, [host]: data.gpus }));
+            // print(data)
+            if ("error" in data) {
+              console.log(data.cpu)
+              setGpuData((prev) => ({ ...prev, [host]: { error: data.error } }));
+            } else {
+              setGpuData((prev) => ({ ...prev, [host]: data.gpus }));
+            }
+            setCpuData((prev) => ({ ...prev, [host]: data.cpu }));
             setLastUpdated((prev) => ({
               ...prev,
               [host]: new Date().toLocaleTimeString(),
@@ -65,7 +74,7 @@ function App() {
     };
 
     fetchData();
-    const interval = setInterval(fetchData, 10000);
+    const interval = setInterval(fetchData, 30000);
     return () => clearInterval(interval);
   }, []);
 
@@ -107,43 +116,72 @@ function App() {
               border="2px solid"
               borderColor="gray.200"
               borderRadius="lg"
-              boxShadow="md"
+              boxShadow="sm"
               p={0}
               bg="white"
               _dark={{ bg: "gray.800", borderColor: "gray.700" }}
               _hover={{ boxShadow: "md", transform: "translateY(-2px)", transition: "all 0.2s" }}
             >              
 
-            <Card.Header>
+            <Card.Header pb={1} mb={0}>
               <Flex justify="space-between" align="center" w="100%">
                 <HStack p={1}>
-                  {gpuData[host] ? (
-                    <Status.Root colorPalette="green"><Status.Indicator /></Status.Root>
+                  {gpuData[host] === null ? (
+                    <Status.Root colorPalette="orange">
+                      <Status.Indicator />
+                    </Status.Root>
+                  ) : gpuData[host] && "error" in gpuData[host] ? (
+                    <Status.Root colorPalette="red">
+                      <Status.Indicator />
+                    </Status.Root>
                   ) : (
-                    <Status.Root colorPalette="red"><Status.Indicator /></Status.Root>
+                    <Status.Root colorPalette="green">
+                      <Status.Indicator />
+                    </Status.Root>
                   )}
+
                   <Heading size="sm">{host}</Heading>
                   <Image src={`/icons/${host}.gif`} boxSize="35px" />
                 </HStack>
-                <Text fontSize="2xs" color="gray.500">
-                  {lastUpdated[host] || "—"}
-                </Text>
+                <Badge colorPalette="cyan">GPU</Badge>
+      
               </Flex>
             </Card.Header>
+            <Separator />
 
-            <Card.Body p={5}>
+            <Card.Body pt={2} mb={3}>
+
+            {/* <Tabs.Root defaultValue="GPU" alignContent="right"> */}
+              {/* <Flex justify="flex-end" mb={2}>
+
+              <Tabs.List align="end">
+              <Tabs.Trigger value="GPU">
+                <Text fontSize="xs">GPUs</Text>
+              </Tabs.Trigger>
+              <Tabs.Trigger value="CPU">
+                <Text fontSize="xs">CPUs</Text>
+              </Tabs.Trigger>
+              </Tabs.List>
+              </Flex> */}
+
+            {/* <Tabs.Content value="GPU"> */}
+            {/* GPU Tab */}
             {!gpuData[host] ? (
-                  <Spinner />
-                ) : gpuData[host].length === 0 ? (
-                  <Text color="red.400">
-                    No GPU data returned from <b>{host}</b>. Host may be down or unresponsive.
-                  </Text>
+                <Spinner />
+                ) : gpuData[host].error ? (
+                  <Alert.Root status="error">
+                    <Alert.Indicator />
+                    <Alert.Content>
+                      <Alert.Title>Error</Alert.Title>
+                      <Alert.Description>{gpuData[host].error}</Alert.Description>
+                    </Alert.Content>
+                  </Alert.Root>
                 ) : (
                 <Box overflowX="auto" whiteSpace="nowrap">
                   <Table.Root fontSize="xs" minW="600px">
                     <Table.Header>
                       <Table.Row fontWeight={700}>
-                        <Table.Cell px={2} py={1}>GPU</Table.Cell>
+                        <Table.Cell px={2} py={1}>Rank</Table.Cell>
                         <Table.Cell px={2} py={1}>Card</Table.Cell>
                         <Table.Cell px={2} py={1}>Temp.</Table.Cell>
                         <Table.Cell px={2} py={1}>Memory</Table.Cell>
@@ -170,8 +208,48 @@ function App() {
                   </Table.Root>
                 </Box>
               )}
-            </Card.Body>
+            {/* </Tabs.Content> */}
+            {/* <Tabs.Content value="CPU"> */}
+            {/* {cpuData[host] && (
+                          <Box  pl={2} pr={5}>
+                            <Flex fontSize="xs" fontWeight="semibold" mb={1} gap={4}>
+                              <Text whiteSpace="nowrap">
+                                CPU ({cpuData[host].n_cpus} CORES)
+                              </Text>
+                              <Text>{sumCPU(cpuData[host].cpu_usage).toFixed(0)}%</Text>
+                              <Text>
+                                {sumMem(cpuData[host].mem_usage).toFixed(0)} /{" "}
+                                {cpuData[host].total_mem.toFixed(0)} (GB)
+                              </Text>
+                            </Flex>
 
+                            {Object.keys(cpuData[host].cpu_usage)
+                              .filter(
+                                (name) =>
+                                  cpuData[host].cpu_usage[name] > 0 ||
+                                cpuData[host].mem_usage[name] > 0
+                              )
+                              .map((name) => (
+                                <Flex key={name} fontSize="2xs" gap={4}>
+                                  <Text w="80px" isTruncated>{name}</Text>
+                                  <Text>{cpuData[host].cpu_usage[name].toFixed(0)}%</Text>
+                                  <Text>
+                                    {cpuData[host].mem_usage[name].toFixed(0)} /{" "}
+                                    {cpuData[host].total_mem.toFixed(0)} (GB)
+                                  </Text>
+                                </Flex>
+                              ))}
+                          </Box>
+                        )} */}
+                {/* </Tabs.Content> */}
+            {/* </Tabs.Root> */}
+
+            <Text fontSize="2xs" color="gray.500" position="absolute"
+                    bottom="10px"
+                    right="25px">
+                  {lastUpdated[host] || "—"}
+                </Text>
+            </Card.Body>
             </Card.Root>
           ))}
         </SimpleGrid>
