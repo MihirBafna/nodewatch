b8f3ff6968a2759afd01ea5a963a601aa87be3b5
MihirBafna <mihir.bafna@gmail.com>
2026-01-01 18:04:38 -0800

    try

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nodewatch/',
+  // base: '/nodewatch/',
+  base: 'https://bergerlabmit.github.io/nodewatch',
+  plugins: [react()],
+  server: {
+    host: true,
+  },
   
 })
